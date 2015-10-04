f2c21e8403d52d51a6ca612bff65fbd1cbd501fb
irxground <irxnjhtchlnrw@gmail.com>
2015-10-04 23:41:29 +0900

    extract to trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 extern crate libc;
 extern crate lua53_sys as luac;
 
+pub mod values;
+
 use std::ffi::CString;
 use std::ffi::CStr;
-use libc::{c_char};
+use libc::{ c_char };
+
+use values::{ Read, Write };
 
 fn with_cstr<T, F>(str: &str, f: F) -> T where F: Fn(*const c_char) -> T {
     let cstr = CString::new(str).unwrap();
@@ -30,7 +34,7 @@ impl Lua {
         unsafe { luac::luaopen_base(self.ptr) != 0 }
     }
 
-    pub fn get_top(&mut self) -> i32 {
+    pub fn get_top_index(&mut self) -> i32 {
         unsafe { luac::lua_gettop(self.ptr) }
     }
 
@@ -40,29 +44,22 @@ impl Lua {
     }
     */
 
-    pub fn set_number<T: AsRef<str>>(&mut self, name: T, value: f64) {
+    pub fn get<T: Read>(&mut self, name: &str) -> Option<T> {
+        let success = with_cstr(name, |str| {
+            unsafe { luac::lua_getglobal(self.ptr, str) != 0 }
+        });
+        if ! success {
+            return None;
+        }
+        return T::read(self, -1);
+    }
+
+    pub fn set<T: Write>(&mut self, name: &str, value: T) {
+        value.write_top(self);
         unsafe {
-            luac::lua_pushnumber(self.ptr, value);
             with_cstr(name.as_ref(), |str| {
                 luac::lua_setglobal(self.ptr, str);
             });
-        }
-    }
-
-    pub fn get_number<T: AsRef<str>>(&mut self, name: T) -> Option<f64> {
-        unsafe {
-            let success = with_cstr(name.as_ref(), |str| {
-                luac::lua_getglobal(self.ptr, str)
-            });
-            if success == 0{
-                return None;
-            }
-            let mut isnum = 0;
-            let lua_number = luac::lua_tonumberx(self.ptr, -1, &mut isnum);
-            if isnum == 0 {
-                return None;
-            }
-            return Some(lua_number);
         }
     }
 
@@ -73,7 +70,7 @@ impl Lua {
         }
     }
 
-    pub fn run<T: AsRef<str>>(&mut self, code: T) -> Result<(), String> {
+    pub fn run(&mut self, code: &str) -> Result<(), String> {
         unsafe {
             let code = with_cstr(code.as_ref(), |str| {
                 luac::luaL_loadstring(self.ptr, str)
@@ -95,11 +92,11 @@ impl Lua {
 #[test]
 fn it_works() {
     let mut lua = Lua::new();
-    lua.set_number("a", 100.0);
-    assert_eq!(Some(100.0), lua.get_number("a"));
-    assert_eq!(None, lua.get_number("b"));
+    lua.set("a", 100.0);
+    assert_eq!(Some(100.0), lua.get("a"));
+    assert_eq!(None, lua.get::<f64>("b"));
 
-    let ok = lua.run("c = a * 2");
-    assert!(ok);
-    assert_eq!(Some(200.0), lua.get_number("c"));
+    let ret = lua.run("c = a * 2");
+    assert!(ret.is_ok());
+    assert_eq!(Some(200.0), lua.get("c"));
 }
---
+++ src/values.rs
@@ -0,0 +1,30 @@
+extern crate lua53_sys as luac;
+
+use Lua;
+
+/// Luaスタックから読み込み可能
+pub trait Read {
+    fn read(lua: &mut Lua, idx: i32) -> Option<Self>;
+}
+
+/// Luaスタックに書き込み可能
+pub trait Write {
+    fn write_top(&self, lua: &mut Lua);
+}
+
+impl Read for f64 {
+    fn read(lua: &mut Lua, idx: i32) -> Option<f64> {
+        let mut isnum = 0;
+        let lua_number = unsafe { luac::lua_tonumberx(lua.ptr, idx, &mut isnum) };
+        if isnum == 0 {
+            return None;
+        }
+        return Some(lua_number);
+    }
+}
+
+impl Write for f64 {
+    fn write_top(&self, lua: &mut Lua) {
+        unsafe { luac::lua_pushnumber(lua.ptr, *self) };
+    }
+}
