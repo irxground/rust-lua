45dbc58116ca60adf98e0a24afc0a17673556e83
irxground <irxnjhtchlnrw@gmail.com>
2017-01-01 20:47:21 +0900

    refactoring

--- src/lua.rs
+++ src/lua.rs
@@ -65,13 +65,6 @@ impl Lua {
 
     pub fn set_fn<F: FnMut() -> ()>(&mut self, name: &str, f: F) {
         use std::mem::size_of;
-        use std::ptr::drop_in_place;
-
-        unsafe extern "C" fn call_drop<F: FnMut() -> ()>(ptr: LuaPtr) -> i32 {
-            let f = luac::lua_touserdata(ptr, luac::lua_upvalueindex(1)) as *mut F;
-            drop_in_place(f);
-            return 0;
-        }
 
         unsafe extern "C" fn callback<F: FnMut() -> ()>(ptr: LuaPtr) -> i32 {
             let f = luac::lua_touserdata(ptr, luac::lua_upvalueindex(1)) as *mut F;
@@ -85,20 +78,11 @@ impl Lua {
             let loc = luac::lua_newuserdata(self.ptr, size) as *mut F;
             debug_assert!(self.current_stack_size() == 1);
             *loc = f;
-            // metatable
-            let meta_table_name = format!("meta-{:p}", &call_drop::<F>);
-            let meta_table_name = CString::new(meta_table_name).unwrap();
-            luac::luaL_newmetatable(self.ptr, meta_table_name.as_ptr());
-            debug_assert!(self.current_stack_size() == 2);
-            "__gc".write_to_stack(self);
-            luac::lua_pushcclosure(self.ptr, Some(call_drop::<F>), 0);
-            debug_assert!(self.current_stack_size() == 4);
-            luac::lua_rawset(self.ptr, -3);
-            debug_assert!(self.current_stack_size() == 2);
             // set metatable
+            self.push_metatable_for_gc::<F>();
             luac::lua_setmetatable(self.ptr, -2);
             debug_assert!(self.current_stack_size() == 1);
-            // TODO support finalizer
+            // set global
             luac::lua_pushcclosure(self.ptr, Some(callback::<F>), 1);
             let name = CString::new(name).unwrap();
             luac::lua_setglobal(self.ptr, name.as_ptr());
@@ -111,6 +95,28 @@ impl Lua {
 
     fn pop_stack(&self, n: i32) {
         unsafe { luac::lua_settop(self.ptr, -1 - n) }
+    }
+
+    fn push_metatable_for_gc<T>(&mut self) {
+        use std::ptr::drop_in_place;
+
+        unsafe extern "C" fn call_drop<T>(ptr: LuaPtr) -> i32 {
+            let item = luac::lua_touserdata(ptr, luac::lua_upvalueindex(1)) as *mut T;
+            drop_in_place(item);
+            return 0;
+        }
+
+        unsafe {
+            let meta_table_name = format!("meta-{:p}", &call_drop::<T>);
+            let meta_table_name = CString::new(meta_table_name).unwrap();
+            luac::luaL_newmetatable(self.ptr, meta_table_name.as_ptr());
+            debug_assert!(self.current_stack_size() == 2);
+            "__gc".write_to_stack(self);
+            luac::lua_pushcclosure(self.ptr, Some(call_drop::<T>), 0);
+            debug_assert!(self.current_stack_size() == 4);
+            luac::lua_rawset(self.ptr, -3);
+            debug_assert!(self.current_stack_size() == 2);
+        }
     }
 }
 
